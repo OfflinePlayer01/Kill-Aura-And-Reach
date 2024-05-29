8847efd23be299d5b5bfe0329bd9db03cbdfa310
OfflinePlayer01 <168975740+OfflinePlayer01@users.noreply.github.com>
2024-05-29 21:01:38 +0300

    Delete Executed_Text_GUI.lua

--- Executed_Text_GUI.lua
+++
@@ -1,6 +0,0 @@
---[[
-By 917Dream | By 917Dream | By 917Dream | By 917Dream | By 917Dream | By 917Dream | By 917Dream | By 917Dream | By 917Dream | By 917Dream | By 917Dream | 
-My Discord Name : _offlineplayer_ | My Discord Name : _offlineplayer_ | My Discord Name : _offlineplayer_ | My Discord Name : _offlineplayer_ | My Discord Name : _offlineplayer_ | My Discord Name : _offlineplayer_ | 
-]]
-loadstring(game:HttpGet("https://raw.githubusercontent.com/OfflinePlayer01/Text_Box_GUI/main/Screen_GUI.lua", true))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/OfflinePlayer01/Text_Box_GUI/main/Up_Screen_GUI.lua", true))()
